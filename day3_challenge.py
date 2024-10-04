3f3ee58e699a6dfbfd34800b1440634432d0c7ed
Nowak <vnowak@cps.k12.il.us>
2024-10-04 11:40:13 -0600

    New Challenge

---
+++ day3_challenge.py
@@ -0,0 +1,33 @@
+#Victoria Nowak, Angela Macias, Kaleb svenningsen, Oscar Colotl-Garista, Nathan Dennis 
+
+#step 1: Ask the user for a piece of text
+text= input("Please enter a text: ")
+print(text)
+
+#step 2: Ask the user for 3 random letters
+letter1= input("enter a letter of your choice: ")
+letter2= input("enter another letter: ")
+letter3 = input("enter one last random letter:  ")
+
+#step 3: Count how many times the 3 random letters appear in the text?
+occurance1 = letter1.count(letter1)
+occurance2 = letter2.count(letter2)
+occurance3 = letter3.count(letter3)
+print(occurance1)
+print(occurance2)
+print(occurance3)
+
+# step 4: How many words are in the whole text?
+text_tuple= tuple(text)
+print(text_tuple)
+
+#step 5: Invert the whole text
+
+# step 6 find the 1st and last words of the text
+
+# step 7 is the word "python" in the text
+
+
+
+
+
